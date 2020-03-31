264504ff9179e08e7356ed59306d9bfdbe8b0cde
WULIbinbin <920658022@qq.com>
2020-03-31 11:08:55 +0800

    feat:fix

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,17 +1,17 @@
-import Home from "../pages/Home";
-import About from "../pages/About";
-import ListDemo from "../pages/List";
-import Login from "../pages/Login/Index";
+import Home from "../pages/home";
+import About from "../pages/about";
+import List from "../pages/list";
+import Login from "../pages/login/index";
 
 export const routePath = [
   { path: '/', exact: true, name: '首页', component: Home, },
   { path: '/about', name: '关于', component: About, },
-  { path: '/list', name: '列表', component: ListDemo, },
+  { path: '/list', name: '列表', component: List, },
   { path: '/login', name: '登录', component: Login, },
 ]
 
 export const tabPath = [
   { path: '/', name: '首页', component: Home, },
   { path: '/about', name: '关于', component: About, },
-  { path: '/list', name: '列表', component: ListDemo, },
+  { path: '/list', name: '列表', component: List, },
 ]
